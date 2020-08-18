d2dcca6ba69e53c053509ebc08ecd0eeea450d21
Josiah Roa <josiahroa18@gmail.com>
2020-08-18 14:58:46 -0600

    Day 3, Step 10 MVP

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -12,11 +12,16 @@ class CPU:
         self.pc = 0
         self.running = True
 
+        # Set the stack pointer to R7
+        self.reg[7] = 0xF4
+
         self.branch_table = {
             0b10000010: self.LDI,
             0b01000111: self.PRN,
             0b00000001: self.HLT,
-            0b10100010: self.MUL
+            0b10100010: self.MUL,
+            0b01000101: self.PUSH,
+            0b01000110: self.POP
         }
 
     # Should accept the address to read and return the value
@@ -45,6 +50,18 @@ class CPU:
         reg_a = self.ram[self.pc + 1]
         reg_b = self.ram[self.pc + 2]
         self.alu('MUL', reg_a, reg_b)
+
+    def PUSH(self):
+        self.reg[7] -= 1
+        register_index = self.ram[self.pc + 1]
+        self.ram[self.reg[7]] = self.reg[register_index]
+        self.pc += 2
+
+    def POP(self):
+        register_index = self.ram[self.pc + 1]
+        self.reg[register_index] = self.ram[self.reg[7]]
+        self.reg[7] += 1
+        self.pc += 2
 
     def load(self, fileName):
         """Load a program into memory."""
@@ -98,19 +115,7 @@ class CPU:
                 self.branch_table[ir]()
             except:
                 raise Exception(f"Unknown instruction: {self.ram[self.pc]}")
-            # if self.ram[self.pc] == HLT:
-            #     running = False
-            # elif self.ram[self.pc] == PRN:
-            #     value = self.reg[self.ram[self.pc + 1]]
-            #     print(value)
-            #     self.pc += 2
-            # elif self.ram[self.pc] == LDI:
-            #     index = self.ram[self.pc + 1]
-            #     value = self.ram[self.pc + 2]
-            #     self.reg[index] = value
-            #     self.pc += 3
-            # else:
-            #     raise Exception(f"Unknown instruction: {self.ram[self.pc]}")
+
             
 
 
